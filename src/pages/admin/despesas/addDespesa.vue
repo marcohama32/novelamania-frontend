dd4a70d1268f20573fdd1d3d3c6d433c1e403050
marcohama32 <50961103+marcohama32@users.noreply.github.com>
2023-11-06 15:55:20 +0200

    Update addDespesa.vue fix Bearer

--- src/pages/admin/despesas/addDespesa.vue
+++ src/pages/admin/despesas/addDespesa.vue
@@ -39,7 +39,7 @@ export default {
         const response = await axios.post(externalApiUrl, requestBody, {
           headers: {
             'Content-Type': 'application/json',
-            'Authorization': 'Bearer rfNjFso4uJbzhwl8E9vizqmHEuD7XDmPqfsRx1L62UoTmURGGLAGgJSl9lCPbgy03Q7NwozFYD4r9BFQY5QpvErHximBDU8HE25urVahm0HnB8VyCIobs684XGSN4GjdequePDrG6xUAxxpvmhqZRlGt1tUjUBeBg6kYqp4EnKHsiaBtvd0THGLZbefpT6UaShASQWYNiEPwEon5wtUMaDwnyQEazDu1H2ieN3r8cCVM3hsak59J/1MP07FQjdFbxdCLfA0DuxgpeKpvLs7WrA767WJSB1QZy7hcP1igSGRfd7Zrp6E7gIukdpC0DApqPKa4XsNTo2AMpG4AwiET2WeKvHn539gbwREXf79kZlYdFDCgTc0Zs7OfDx5ZXMCBKHOS/H3tVFJqXTfEfIF5LOzrFU5pPE0HeNBV0Q2vm8qRwQX0RijnvMOGpdcmXb0qoph4oy8Mj+vjRfFRboMAafttDozBhRmWEmeBB3EjYASm1fToQp5ey6ltCiEt8rjL5PlexxB0u3u2LVJQcDzMVNiiq10t1xyw8qtc6BMOyrKVlIANWglRYOKr9saVBVvDFUcCfsghMjUTDeAwHom4A3cSDWmVlNF9Vs/WqCoUzjQCV0BFPDzeAUbQqt7h7OgFno/+D9n5j1eMro0aXbbHNx71u8YmgPJhdixzFhxM1Pw=', // Replace with your actual bearer token
+            'Authorization': 'Bearer', // Replace with your actual bearer token
             'Origin': 'developer.mpesa.vm.co.mz'
 
           },
